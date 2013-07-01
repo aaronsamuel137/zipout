15f5da37d70c253b3ec397dcdd00c392c60544fa
aaronsamuel137 <aaron.davis.samuel@gmail.com>
2013-07-01 11:28:42 -0600

    method stubs

--- src/ZipOutputStream.java
+++ src/ZipOutputStream.java
@@ -1,4 +1,13 @@
 
 public class ZipOutputStream {
-
+  private static final int SIGNATURE = 0x04034b50;
+  
+  private void writeLocalHeader() {
+        
+  }
+    
+  private void writeLocalHeader() {
+        
+  }
+    
 }
