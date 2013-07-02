106fe1badf70201788fc97ad2f67ed324ecb168d
Chris Jordan <chris.jordan@readytalk.com>
2013-07-02 11:39:25 -0600

    updating zipentry with time

--- src/ZipEntry.java
+++ src/ZipEntry.java
@@ -1,6 +1,7 @@
+package src;
 
-
-import java.util.Calendar;
+import src.Calendar.MyCalendar;
+import src.Date;
 
 public class ZipEntry {
   private String name;
@@ -55,29 +56,107 @@ public class ZipEntry {
   }
   
   //Method to get the time frome the system
-  public void setTime(){
-	  //Need to expand on this....
+  public void setTimeDate(){
+	  //Create Date and Calendar objects to populate information
+	  Date modTime = new Date();
+	  MyCalendar modCalendar = new MyCalendar(modTime.getTime());
+	  
+	  //Hour is military time
+	  int timeBits = (modCalendar.HOUR_OF_DAY);
+	  
+	  //Shift bits
+	  timeBits = timeBits << 6;
+	  
+	 //Get the minutes from MyCalendar
+	  int minBits = modCalendar.DAY_OF_MONTH;
+	  
+	  //Mask all but the last 6 bits of minutes
+	  int minMask = 0xFFC0;
+	  minBits = minBits ^ minMask;
+	  
+	  //Add minutes to dateBits
+	  timeBits = timeBits ^ minBits;
+	  
+	  //Shift bits
+	  timeBits = timeBits << 5;
+	  
+	  //Get the seconds from MyCalendar
+	  int secBits = modCalendar.SECOND;
+	  
+	  //Divide seconds by 2
+	  secBits = secBits >> 1;
+	  
+	  //Mask all but the last 5 bits of seconds
+	  int secMask = 0xFFE0;
+	  secBits = secBits ^ secMask;
+	  
+	  //Add minutes to dateBits
+	  timeBits = timeBits ^ secBits;
+	  
+	  //Truncate to short then store
+	  this.MOD_TIME = (short)timeBits;
+	  
+	  //Year is based on offset from 1980
+	  int dateBits = (modCalendar.YEAR -1980);
+	  
+	  //Shift bits over
+	  dateBits = dateBits << 4;
+	  
+	  //Get month from MyCalendar, Jan starts as "0"
+	  dateBits = dateBits & (byte)(modCalendar.MONTH -1);
+	  
+	  //Shift bits over again
+	  dateBits = dateBits << 5;
+	  
+	  //Get the day from MyCalendar
+	  int dayBits = modCalendar.DAY_OF_MONTH;
+	  
+	  //Mask all but the last 5 bits of day
+	  int dayMask = 0xFFE0;
+	  dayBits = dayBits ^ dayMask;
+	  
+	  //Add day to dateBits
+	  dateBits = dateBits ^ dayBits;
+	  
+	  //Truncate to short then store
+	  this.MOD_DATE = (short)dateBits;
+	  
+	  
+	  
+	  
+	  
+	
+	  
+	  
+	  
     
   }
   
-  //Method to set the mi
-  
-  //Method to set the minium version required to open the zip file
-  //Valid values for the compression method are the numbers 10 to 63
-  public boolean setRequiredVersion(short compMethod){
-	  if (compMethod < 10 || compMethod > 63){
+  //Method to set the minimum version required to open the zip file
+  //Valid values for the compression method are the numbers 1.0 to 10.0
+  public boolean setRequiredVersion(float versionFloat){
+	  //Check for valid version numbers
+	  if (versionFloat < 1 || versionFloat > 100){
 		  return false;
 	  }
-	  else{
-		  COMPRESSION_METHOD = compMethod;
-		  return true;
-	  }
+	  
+	  //Convert to short value for storage
+	  versionFloat = versionFloat * 10;
+	  short versionShort = (short)versionFloat;
+	  
+	  //Set value of version
+	  REQ_VERSION = versionShort;
+	  return true;
   }
   
   //Method to set the compression method for the file
-  //Valid values for the compression method are the numbers 0 to 19
+  //Valid values for the compression method are the numbers 0 to 19 and 99
   public boolean setCompressionMethod(short compMethod){
-	  if (compMethod < 0 || compMethod > 19){
+	  if (compMethod == 99){
+		  COMPRESSION_METHOD = compMethod;
+		  return true;
+	  }
+	  else if (compMethod < 0 || compMethod > 19){
 		  return false;
 	  }
 	  else{
